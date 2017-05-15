d8796b4f799d1390e5b5981f0e0747a5a36c78e8
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-15 11:35:30 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,12 +1,13 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist
+    belongs_to :artist
+    has_attached_file :album_cover, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
+    def artist_name
+        self.try(:artist).try(:name)
+    end
 
-  def artist_name
-    self.try(:artist).try(:name)
-  end
-
-  def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
-  end
+    def artist_name=(name)
+        artist = Artist.find_or_create_by(name: name)
+        self.artist = artist
+    end
 end
