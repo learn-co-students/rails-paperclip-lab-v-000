0167694f276480f6c85ab260e372db0190955fca
PhreeMason <mr.omarmason@gmail.com>
2017-06-05 23:23:28 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  has_attached_file :album_cover, default_url: 'http://img.ulximg.com/image/740x493/gallery/1495738697_4e0c603960f0410758eb727ff9154eaf.jpg/1c209630614ee55901eff3fadbeca6eb/1495738697_10ad46b6ba371761b275b2be36f3f325.jpg', styles: { thumb: "100x100>" }
+  validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
 
   def artist_name
     self.try(:artist).try(:name)
@@ -9,4 +11,6 @@ class Song < ActiveRecord::Base
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
   end
+  
+
 end
