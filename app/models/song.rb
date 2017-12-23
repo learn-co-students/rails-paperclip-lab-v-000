789a15c910301f3064464acb4f8e60d5ac69f5e0
jamesonbass1987 <jamesonbass@gmail.com>
2017-12-23 11:58:36 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  has_attached_file :album_cover, default_url: ':style/default-album-cover.jpg', styles: { thumb: "100x100>", original: "400x400" }
+  validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
 
   def artist_name
     self.try(:artist).try(:name)
