81d8cf62cb5c255a2f8cee6760f19019b6f1c2d3
marielfrank <marielbeth@gmail.com>
2017-12-27 09:43:22 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  has_attached_file :album_cover, default_url: 'https://lh5.googleusercontent.com/-_B87VGbuz2E/TYmjdjwV7bI/AAAAAAAAFlA/EY-0D9_UL7c/s1600/AWESOME+FACEBOOK+PROFILE+PICTURES10.jpg', styles: { thumb: "100x100>" }
+  validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
 
   def artist_name
     self.try(:artist).try(:name)
