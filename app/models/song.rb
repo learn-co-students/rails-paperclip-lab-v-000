f567c695a5be3b75ec7e41c0f8522e6d196aebcb
Cynthia Rittenbach <crittenbach@gmail.com>
2018-01-19 10:58:32 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  has_attached_file :album_cover, default_url: 'style/homer.gif', styles: { thumb: "100x100>" }
 
   def artist_name
     self.try(:artist).try(:name)
@@ -9,4 +10,5 @@ class Song < ActiveRecord::Base
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
   end
+
 end
